dcb0bbbcd37d65df030f2417ae79ba86ca2333a6
Andy Sterkowitz <andy@andysterkowitz.com>
2020-06-24 11:01:35 -0700

    First step

--- src/ContactsTable.js
+++ src/ContactsTable.js
@@ -2,20 +2,6 @@ import React, { Component } from "react";
 
 class ContactsTable extends Component {
   render() {
-    const dataRows = this.props.contacts.map(contact => {
-        return (
-            <tr>
-                <td>{`${contact.firstName} ${contact.lastName}`}</td>
-                <td>{contact.phone}</td>
-                <td>{contact.email}</td>
-                <td>{`${contact.addressLine1} ${contact.addressLine2}`}</td>
-                <td>{`${contact.city}, ${contact.state}`}</td>
-                <td>{contact.country}</td>
-                <td>{contact.notes}</td>
-            </tr>
-        )
-    });
-
     return (
       <table className="table table-bordered mt-5">
         <thead className="thead-dark">
@@ -30,7 +16,7 @@ class ContactsTable extends Component {
           </tr>
         </thead>
         <tbody>
-            {dataRows}
+          {/* Display table data here */}
         </tbody>
       </table>
     );
